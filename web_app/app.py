54c9d9ce59f2d3dfea745df935562298d6954e25
Daniel <xpatek08@stud.fit.vutbr.cz>
2020-12-11 14:00:22 +0100

    database

--- web_app/app.py
+++ web_app/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
+from web_app.extensions import db
+from web_app.commands import register_commands
 from web_app.routes import routes
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile("settings.py")
     app.register_blueprint(routes)
+    db.init_app(app)
+    register_commands(app)
     return app
 
---
+++ web_app/models.py
@@ -0,0 +1,38 @@
+from appdata.extensions import db
+import enum
+
+class RoomType(enum.Enum):
+    Economy = 0
+    Business = 1
+    Premium = 2
+
+class CleanupType(enum.Enum):
+    Standartni = 0
+    Preduklid = 1
+    Minibar = 2
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    number = db.Column(db.Integer, nullable=False, unique=True)
+    r_type = db.Column(db.Enum(RoomType), nullable=False)
+
+    cleanups = db.relationship('Cleanup', uselist=False, backref='room', cascade="all, delete-orphan")
+    faults = db.relationship('Fault', uselist=False, backref='room', cascade="all, delete-orphan")
+    discoveries = db.relationship('Discovery', uselist=False, backref='room', cascade="all, delete-orphan")
+
+class Cleanup(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
+    c_type = db.Column(db.Enum(CleanupType), nullable=False)
+    completed = db.Column(db.models.DateTime)
+    to_be_completed = db.Column(db.models.DateTime, nullable=False)
+
+class Fault(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
+    description = db.Column(db.String, nullable=False)
+
+class Discovery(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'))
+    description = db.Column(db.String, nullable=False)
