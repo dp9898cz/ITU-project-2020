a6e3136d2d667907ef772a7fa3183d968e89e671
Daniel <xpatek08@stud.fit.vutbr.cz>
2020-12-11 13:06:00 +0100

    dfhfggfjdfgj

--- web_app/app.py
+++ web_app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-from web_app.routes import main
+from web_app.routes import routes
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(main)
+    app.register_blueprint(routes)
     return app
 
