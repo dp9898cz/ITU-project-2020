f9a7ad8ebde69d43112a1aed11ea5d463d958006
Daniel Čechák <sonaga98@gmail.com>
2021-01-25 12:18:03 +0100

    picovina

--- web_app/static/js/searchRooms.js
+++ web_app/static/js/searchRooms.js
@@ -1,11 +1,9 @@
 const searchField = document.querySelector("#searchField");
-console.log("hez0")
 searchField.addEventListener("keyup",(e) => {
     const searchValue = e.target.value;
-    console.log("searchValue",searchValue);
-    console.log("hez1")
-    if(searchValue.lenght > 0){
-        console.log("hez2")
-        
+
+    if(searchValue.length > 0){
+
+        console.log(searchValue);
     }
 });
