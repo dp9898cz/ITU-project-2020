da5e3830ac9e595e90fa6f2f320fe5e7d2f55309
Daniel Čechák <sonaga98@gmail.com>
2021-01-25 12:59:13 +0100

    s

--- web_app/urls.py
+++ web_app/urls.py
@@ -3,5 +3,5 @@ from . import routes
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('search-rooms',csrf_exempt(routes.search_rooms), name="search_rooms")
+    path('/search-rooms',csrf_exempt(routes.search_rooms), name="search_rooms")
 ]
