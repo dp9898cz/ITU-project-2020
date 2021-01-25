5539f89ed7d0137bc4aa905f9c02932e1ce64755
Daniel Čechák <sonaga98@gmail.com>
2021-01-25 18:54:33 +0100

    s

--- web_app/static/js/searchRooms.js
+++ web_app/static/js/searchRooms.js
@@ -13,37 +13,19 @@ searchField.addEventListener("keyup",(e) => {
             dataType: "json",
             success: function (responce) {
                 console.log(responce.message);   
-                if (responce == "found") {
-                    document.getElementById("pokoj-check").style.boxShadow = "0 0px 0px 0 rgb( 0,255, 0), 0 0px 6px 0 rgb( 0,255, 0)";
+                if (responce.message == "found") {
+                    document.getElementById("searchField").style.boxShadow = "0 0px 0px 0 rgb( 0,255, 0), 0 0px 6px 0 rgb( 0,255, 0)";
                     document.getElementById("error").style.display="none";
-                }else if(responce == "notfound"){
-                    document.getElementById("pokoj-check").style.boxShadow = "0 0px 0px 0 rgb(255, 0, 0), 0 0px 6px 0 rgb(255, 0, 0)";
+                }else if(responce.message == "notfound"){
+                    document.getElementById("searchField").style.boxShadow = "0 0px 0px 0 rgb(255, 0, 0), 0 0px 6px 0 rgb(255, 0, 0)";
                     document.getElementById("error").style.display="block";
                     } 
-                if(responce == "empty"){
-                    document.getElementById("pokoj-check").style.boxShadow = "0 0px 0px 0 rgb( 0,0, 0), 0 0px 0px 0 rgb( 0,0, 0)";
+                if(responce.message == "empty"){
+                    document.getElementById("searchField").style.boxShadow = "0 0px 0px 0 rgb( 0,0, 0), 0 0px 0px 0 rgb( 0,0, 0)";
                     document.getElementById("error").style.display="none";
                 }
             },
         });
-    
-        /*var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                console.log("hej");
-            }
-        };
-        xhttp.open("POST", "/search-rooms", true);
-        xhttp.send();
-            */
-        /*fetch("/search-rooms",{
-            body: searchValue,
-            method: "POST",
 
-        })
-            .then((text)=>text.json())
-            .then((message)=>{
-                console.log(message);
-        });*/
     }
 });
