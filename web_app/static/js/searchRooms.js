2492180be78276514f29b70dfa8eee0fddbc5c5a
Daniel Čechák <sonaga98@gmail.com>
2021-01-25 12:15:44 +0100

    s

--- web_app/static/js/searchRooms.js
+++ web_app/static/js/searchRooms.js
@@ -2,9 +2,10 @@ const searchField = document.querySelector("#searchField");
 console.log("hez0")
 searchField.addEventListener("keyup",(e) => {
     const searchValue = e.target.value;
+    console.log("searchValue",searchValue);
     console.log("hez1")
     if(searchValue.lenght > 0){
         console.log("hez2")
-        console.log("searchValue",searchValue);
+        
     }
 });
