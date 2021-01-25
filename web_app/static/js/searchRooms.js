4dcd27d65525c75c4886eaa00a1c18534539587c
Daniel Čechák <sonaga98@gmail.com>
2021-01-25 11:57:16 +0100

    s

--- web_app/static/js/searchRooms.js
+++ web_app/static/js/searchRooms.js
@@ -1,9 +1,9 @@
 const searchField = document.querySelector("#searchField");
 
-searchField.addEventListener("keyup",(e)=> {
+searchField.addEventListener("keyup",(e) => {
     const searchValue = e.target.value;
 
-    if(searchValue.lenght>0){
-        console.log('searchValue',object)
+    if(searchValue.lenght > 0){
+        console.log('searchValue',searchValue)
     }
 })
